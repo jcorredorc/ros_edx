9b5c3d0f110be0f6409a358b66fd9c5e2c1dfd3e
jcorredorc <jcorredorc@github.com>
2022-05-04 17:12:27 -0500

    feat: singularity based week 5 assignement, first attempt. An issue with several white boxes in gazebo

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Read a ultrasound sensor connected to an arduino board (see [here](https://githu
 
 
 ```
-roslaunch udemy_ros_beginners arduino_hcsr04.launch
+$roslaunch udemy_ros_beginners arduino_hcsr04.launch
 ```
 
 Then publish the data in the custom message `SensorInformation.msg`
@@ -16,7 +16,7 @@ This script `sensor_info_publisher.py` is modified from the course objective, it
 
 
 ```
-rosrun ros_edx sensor_info_publisher.py
+$rosrun ros_edx sensor_info_publisher.py
 ```
 
 ### Assignments
@@ -24,7 +24,7 @@ rosrun ros_edx sensor_info_publisher.py
 1a. this script subscribe to `/sensor_info` and check if the data is less than 0.1.
 
 ```
-rosrun ros_edx week1_assignment1_part1.py 
+$rosrun ros_edx week1_assignment1_part1.py 
 ```
 
 1b. Creates a custom message
@@ -54,26 +54,26 @@ Follow the next ROS tutorials:
 
 see [join element](http://wiki.ros.org/urdf/XML/joint)
 
-check the folder instalation with
+check the tutorial instalation with
 
 ```
-roscd urdf_tutorial
+$roscd urdf_tutorial
 ```
 
 check for errors in urdf files, first conver from xacro to urdf
 
 ```
-rosrun xacro xacro /path/to/robot.xacro > robot.urdf
-check_urdf robot.urdf
+$rosrun xacro xacro /path/to/robot.xacro > robot.urdf
+$check_urdf robot.urdf
 ```
 
-1. `sudo apt install liburdfdom-tools`
+1. `$ sudo apt install liburdfdom-tools`
 
-2. `rosrun xacro xacro --inorder ${find hrwros_support}/urdf/hrwros.xacro > hrwros.urdf`
+2. `$ rosrun xacro xacro --inorder ${find hrwros_support}/urdf/hrwros.xacro > hrwros.urdf`
 
-3. `check_urdf hrwros.urdf `
+3. `$ check_urdf hrwros.urdf `
 
-4. `roslaunch hrwros_suport  visualize_hrwros.launch`
+4. `$ roslaunch hrwros_suport  visualize_hrwros.launch`
 
 
 ### Assignments
@@ -82,11 +82,11 @@ Require the singularity environtment available in the course. See [singularity f
 
 `git clone --depth 1 --branch week4  https://github.com/jcorredorc/ros_edx.git `
 
-1. `roslaunch hrwros_week2_assignment visualize_hrwros_assignment1.launch`
+1. `$ roslaunch hrwros_week2_assignment visualize_hrwros_assignment1.launch`
 
-2. `roslaunch hrwros_week2_assignment visualize_hrwros_assignment2.launch`
+2. `$ roslaunch hrwros_week2_assignment visualize_hrwros_assignment2.launch`
 
-3. `roslaunch hrwros_week2_assignment visualize_hrwros_assignment3.launch`
+3. `$ roslaunch hrwros_week2_assignment visualize_hrwros_assignment3.launch`
 
  
 
@@ -105,27 +105,199 @@ Require the singularity environtment available in the course. See [singularity f
 `git clone --depth 1 --branch week4  https://github.com/jcorredorc/ros_edx.git `
 
 
-1. `roslaunch week4_moveit_config setup_assistant.launch`
+1. `$ roslaunch week4_moveit_config setup_assistant.launch`
 
 2. 
 
 ```  
-    roslaunch hrwros_week4_assignment hrwros_week4_environment.launch
-    roscd hrwros_week4_assignment/scripts
-    rosrun hrwros_week4 hrwros_moveit_commander_cmdline
+    $ roslaunch hrwros_week4_assignment hrwros_week4_environment.launch
+    $ roscd hrwros_week4_assignment/scripts
+    $ rosrun hrwros_week4 hrwros_moveit_commander_cmdline
     > load week4_assignment2_script
 ```
 
 3. 
-    `roslaunch hrwros_week4_assignment hrwros_week4_environment.launch`
 
-    `roslaunch hrwros_week4_assignment week4_assignment3.launch`
-
+```
+    $ roslaunch hrwros_week4_assignment hrwros_week4_environment.launch
+    $ roslaunch hrwros_week4_assignment week4_assignment3.launch
+```
 
 ## Week 5
 
+```
+$ roslaunch hrwros_gazebo hrwros_environment.launch gui:=false rviz:=true
+```
+
+### Assignments
 
 
+Require the singularity environtment available in the course. See [singularity folder](singularity/)
+
+1. 
+
+```
+$ roslaunch hrwros_week5_assignment week5_assignment1.launch
+```
+
+```
+$ rostopic list
+...
+/hrwros/new_logical_camera_1
+/hrwros/new_logical_camera_2
+...
+```
+
+```
+> rostopic echo /hrwros/new_logical_camera_1 -n1
+models: 
+  - 
+    type: "workcell"
+    pose: 
+      position: 
+        x: 2.000004407832632
+        y: -1.8000000000000003
+        z: -1.1999926535816976
+      orientation: 
+        x: -0.7071080798594737
+        y: 0.0
+        z: 0.0
+        w: 0.7071054825112364
+pose: 
+  position: 
+    x: 1.2
+    y: 1.8
+    z: 2.0
+  orientation: 
+    x: 0.0
+    y: 0.7071080798594737
+    z: 0.0
+    w: 0.7071054825112364
+---
+Singularity HRWROS:~>
+```
+
+```
+> rostopic echo /hrwros/new_logical_camera_2 -n1
+models: 
+  - 
+    type: "robot2_pedestal"
+    pose: 
+      position: 
+        x: 1.7999695123854997
+        y: 1.23
+        z: 8.300006611713194
+      orientation: 
+        x: -0.7071080798594737
+        y: 0.0
+        z: 0.0
+        w: 0.7071054825112364
+  - 
+    type: "workcell"
+    pose: 
+      position: 
+        x: 1.7999695123854997
+        y: 1.23
+        z: 8.300006611713194
+      orientation: 
+        x: -0.7071080798594737
+        y: 0.0
+        z: 0.0
+        w: 0.7071054825112364
+  - 
+    type: "robot2"
+    pose: 
+      position: 
+        x: 1.0999981220770239
+        y: -0.2699999319879862
+        z: 0.5000040640874971
+      orientation: 
+        x: -0.5001868618064235
+        y: -0.49980823898733373
+        z: 0.4997935181189006
+        w: 0.5002112221481584
+pose: 
+  position: 
+    x: -8.3
+    y: -1.23
+    z: 1.8
+  orientation: 
+    x: 0.0
+    y: 0.7071080798594737
+    z: 0.0
+    w: 0.7071054825112364
+---
+Singularity HRWROS:~> 
+```
+
+2. 
+
+```
+roslaunch hrwros_week5_assignment week5_assignment1.launch
+```
+
+```
+rosrun tf2_tools view_frames.py
+```
+
+```
+> rosrun tf2_tools echo.py new_logical_camera_2_frame camera_rgb_frame
+At time 4488.7, (current time 4488.762)
+- Translation: [1.503, 1.006, 4.214]
+- Rotation: in Quaternion [-0.177, -0.685, 0.177, 0.685]
+            in RPY (radian) [-1.571, -1.065, 1.571]
+            in RPY (degree) [-90.000, -61.006, 90.000]
+At time 4489.7, (current time 4489.762)
+- Translation: [1.503, 1.006, 4.214]
+- Rotation: in Quaternion [-0.177, -0.685, 0.177, 0.685]
+            in RPY (radian) [-1.571, -1.065, 1.571]
+            in RPY (degree) [-90.000, -60.996, 90.000]
+At time 4490.7, (current time 4490.762)
+- Translation: [1.503, 1.006, 4.214]
+- Rotation: in Quaternion [-0.177, -0.685, 0.177, 0.685]
+            in RPY (radian) [-1.571, -1.065, 1.571]
+            in RPY (degree) [-90.000, -60.995, 90.000]
+```
+
+2.1.
+```
+$ roslaunch hrwros_week5_assignment week5_assignment2_part1.launch
+```
+
+2.2.
+
+
+```
+$ rosrun tf2_ros static_transform_publisher 0 0 0.3 0 0 0 1 plate_top_link turtlebot_object_top
+```
+
+```
+$ roslaunch turtlebot_teleop keyboard_teleop.launch
+```
+```
+$ roslaunch hrwros_week5_assignment week5_assignment2_part2.launch
+```
+
+2.3.
+
+```
+$ roslaunch hrwros_week5_assignment week5_assignment3.launch
+```
+
+```
+$ rosservice call /spawn_object_on_turtlebot
+```
+
+```
+$ rosrun hrwros_week4 hrwros_moveit_commander_cmdline
+
+> use robot2
+> go R2PreGrasp
+```
+
+```
+$ rosrun hrwros_week5_assignment week5_assignment3.py
+```
 
 
 
--- singularity/hrwros_gazebo/include/hrwros_gazebo/urdf_creator.h
+++ singularity/hrwros_gazebo/include/hrwros_gazebo/urdf_creator.h
@@ -5,154 +5,150 @@
 
 namespace hrwros
 {
-namespace simulation
-{
-
-struct InertiaTag
-{
-  InertiaTag(const std::string& ixx = "1",
-             const std::string& iyy = "1",
-             const std::string& izz = "1",
-             const std::string& ixy = "0",
-             const std::string& iyz = "0",
-             const std::string& ixz = "0")
-    : self(new TiXmlElement ("inertia"))
+  namespace simulation
   {
-    self->SetAttribute("ixx", ixx);
-    self->SetAttribute("iyy", iyy);
-    self->SetAttribute("izz", izz);
-    self->SetAttribute("ixy", ixy);
-    self->SetAttribute("iyz", iyz);
-    self->SetAttribute("ixz", ixz);
-  }
 
-  TiXmlElement* self;
-};
+    struct InertiaTag
+    {
+      InertiaTag(const std::string &ixx = "1",
+                 const std::string &iyy = "1",
+                 const std::string &izz = "1",
+                 const std::string &ixy = "0",
+                 const std::string &iyz = "0",
+                 const std::string &ixz = "0")
+          : self(new TiXmlElement("inertia"))
+      {
+        self->SetAttribute("ixx", ixx);
+        self->SetAttribute("iyy", iyy);
+        self->SetAttribute("izz", izz);
+        self->SetAttribute("ixy", ixy);
+        self->SetAttribute("iyz", iyz);
+        self->SetAttribute("ixz", ixz);
+      }
 
-struct OriginTag
-{
-  OriginTag(const std::string& xyz = "0 0 0",
-            const std::string& rpy = "0 0 0")
-    : self(new TiXmlElement ("origin"))
-  {
-    self->SetAttribute("xyz", xyz);
-    self->SetAttribute("rpy", rpy);
-  }
+      TiXmlElement *self;
+    };
 
-  TiXmlElement* self;
-};
+    struct OriginTag
+    {
+      OriginTag(const std::string &xyz = "0 0 0",
+                const std::string &rpy = "0 0 0")
+          : self(new TiXmlElement("origin"))
+      {
+        self->SetAttribute("xyz", xyz);
+        self->SetAttribute("rpy", rpy);
+      }
 
-struct GeometryTag
-{
-  GeometryTag(const std::string& mesh_filename)
-    : self(new TiXmlElement ("geometry"))
-    , mesh(new TiXmlElement ("mesh"))
-  {
-    mesh->SetAttribute("filename", mesh_filename);
-    self->LinkEndChild(mesh);
-  }
+      TiXmlElement *self;
+    };
 
-  TiXmlElement* self;
-  TiXmlElement* mesh;
-};
+    struct GeometryTag
+    {
+      GeometryTag(const std::string &mesh_filename)
+          : self(new TiXmlElement("geometry")), mesh(new TiXmlElement("mesh"))
+      {
+        mesh->SetAttribute("filename", mesh_filename);
+        self->LinkEndChild(mesh);
+      }
 
-struct MaterialTag
-{
-  MaterialTag(const std::string& color_name = "medium_gray",
-              const std::string& color_string = "0.5 0.5 0.5 1.0")
-    : self(new TiXmlElement ("material"))
-    , color(new TiXmlElement ("color"))
-  {
-    color->SetAttribute("rgba", color_string);
-    self->SetAttribute("name", color_name);
-    self->LinkEndChild(color);
-  }
+      TiXmlElement *self;
+      TiXmlElement *mesh;
+    };
 
-  MaterialTag(const std::string& color_name)
-    : self(new TiXmlElement ("material"))
-  {
-    self->SetAttribute("name", color_name);
-  }
+    struct MaterialTag
+    {
+      MaterialTag(const std::string &color_name = "medium_gray",
+                  const std::string &color_string = "0.5 0.5 0.5 1.0")
+          : self(new TiXmlElement("material")), color(new TiXmlElement("color"))
+      {
+        color->SetAttribute("rgba", color_string);
+        self->SetAttribute("name", color_name);
+        self->LinkEndChild(color);
+      }
 
-  TiXmlElement* self;
-  TiXmlElement* color;
-};
+      MaterialTag(const std::string &color_name)
+          : self(new TiXmlElement("material"))
+      {
+        self->SetAttribute("name", color_name);
+      }
 
-struct VisualTag
-{
-  VisualTag(const GeometryTag& geometry_tag,
-            const MaterialTag& material_tag = MaterialTag(),
-            const OriginTag& origin_tag = OriginTag())
-    : self(new TiXmlElement ("visual"))
-  {
-    self->LinkEndChild(origin_tag.self);
-    self->LinkEndChild(geometry_tag.self);
-    self->LinkEndChild(material_tag.self);
-  }
+      TiXmlElement *self;
+      TiXmlElement *color;
+    };
 
-  TiXmlElement* self;
-};
+    struct VisualTag
+    {
+      VisualTag(const GeometryTag &geometry_tag,
+                const MaterialTag &material_tag = MaterialTag(),
+                const OriginTag &origin_tag = OriginTag())
+          : self(new TiXmlElement("visual"))
+      {
+        self->LinkEndChild(origin_tag.self);
+        self->LinkEndChild(geometry_tag.self);
+        self->LinkEndChild(material_tag.self);
+      }
 
-struct CollisionTag
-{
-  CollisionTag(const GeometryTag& geometry_tag,
-               const MaterialTag& material_tag = MaterialTag(),
-               const OriginTag& origin_tag = OriginTag())
-    : self(new TiXmlElement ("collision"))
-  {
-    self->LinkEndChild(origin_tag.self);
-    self->LinkEndChild(geometry_tag.self);
-    self->LinkEndChild(material_tag.self);
-  }
+      TiXmlElement *self;
+    };
 
-  TiXmlElement* self;
-};
+    struct CollisionTag
+    {
+      CollisionTag(const GeometryTag &geometry_tag,
+                   const MaterialTag &material_tag = MaterialTag(),
+                   const OriginTag &origin_tag = OriginTag())
+          : self(new TiXmlElement("collision"))
+      {
+        self->LinkEndChild(origin_tag.self);
+        self->LinkEndChild(geometry_tag.self);
+        self->LinkEndChild(material_tag.self);
+      }
 
-struct InertialTag
-{
-  InertialTag(const std::string& mass_value = "0.2",
-              const InertiaTag& inertia_tag = InertiaTag(),
-              const OriginTag& origin_tag = OriginTag())
-    : self(new TiXmlElement ("inertial"))
-    , mass(new TiXmlElement ("mass"))
-  {
-    mass->SetAttribute("value", mass_value);
-    self->LinkEndChild(mass);
-    self->LinkEndChild(inertia_tag.self);
-    self->LinkEndChild(origin_tag.self);
-  }
+      TiXmlElement *self;
+    };
 
-  TiXmlElement* self;
-  TiXmlElement* mass;
-};
+    struct InertialTag
+    {
+      InertialTag(const std::string &mass_value = "0.2",
+                  const InertiaTag &inertia_tag = InertiaTag(),
+                  const OriginTag &origin_tag = OriginTag())
+          : self(new TiXmlElement("inertial")), mass(new TiXmlElement("mass"))
+      {
+        mass->SetAttribute("value", mass_value);
+        self->LinkEndChild(mass);
+        self->LinkEndChild(inertia_tag.self);
+        self->LinkEndChild(origin_tag.self);
+      }
 
-struct RobotTag
-{
-  RobotTag(const std::string& name,
-           const VisualTag& visual_tag,
-           const CollisionTag& collision_tag,
-           const InertialTag& inertial_tag = InertialTag())
-    : self(new TiXmlElement ("robot"))
-    , link(new TiXmlElement ("link"))
-  {
-    self->SetAttribute("name", name);
-    self->SetAttribute("xmlns:xacro", "https://www.ros.org/wiki/xacro");
-    link->SetAttribute("name", name + "_base_link");
+      TiXmlElement *self;
+      TiXmlElement *mass;
+    };
 
-    link->LinkEndChild(visual_tag.self);
-    link->LinkEndChild(collision_tag.self);
-    link->LinkEndChild(inertial_tag.self);
-    self->LinkEndChild(link);
-  }
+    struct RobotTag
+    {
+      RobotTag(const std::string &name,
+               const VisualTag &visual_tag,
+               const CollisionTag &collision_tag,
+               const InertialTag &inertial_tag = InertialTag())
+          : self(new TiXmlElement("robot")), link(new TiXmlElement("link"))
+      {
+        self->SetAttribute("name", name);
+        self->SetAttribute("xmlns:xacro", "https://www.ros.org/wiki/xacro");
+        link->SetAttribute("name", name + "_base_link");
 
-  TiXmlElement* self;
-  TiXmlElement* link;
-};
+        link->LinkEndChild(visual_tag.self);
+        link->LinkEndChild(collision_tag.self);
+        link->LinkEndChild(inertial_tag.self);
+        self->LinkEndChild(link);
+      }
 
-std::string createObjectURDF(const std::string& object_name,
-                             const std::string& mesh_filename);
+      TiXmlElement *self;
+      TiXmlElement *link;
+    };
 
-} // namespace simulation
+    std::string createObjectURDF(const std::string &object_name,
+                                 const std::string &mesh_filename);
+
+  } // namespace simulation
 } // namespace hrwros
 
 #endif // GILBRETH_GAZEBO_URDF_CREATOR_H
